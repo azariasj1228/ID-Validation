33f91e0405dedd7eaee8c76b58f341c7d9423887
Azaria Johnson <azariajohnson2000@gmail.com>
2026-05-08 15:48:55 -0400

    Final cleanup of all duplicates and conflicts

--- Pan Card Tampering Flask App/app.py
+++ Pan Card Tampering Flask App/app.py
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 from app import app
 
 if __name__ == '__main__':
-=======
-from app import app
-
-if __name__ == '__main__':
->>>>>>> aac06878b7a1a810ea638f269f262f646de801d0
-   app.run()
+    app.run()
--- Pan Card Tampering Flask App/app/__init__.py
+++ Pan Card Tampering Flask App/app/__init__.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import Flask
 
 app = Flask(__name__)
@@ -11,18 +10,4 @@ elif env == "testing":
 else:
     app.config.from_object("config.DevelopmentConfig")
 
-=======
-from flask import Flask
-
-app = Flask(__name__)
-env = app.config.get("ENV", "development")
-
-if env == "production":
-    app.config.from_object("config.ProductionConfig")
-elif env == "testing":
-    app.config.from_object("config.TestingConfig")
-else:
-    app.config.from_object("config.DevelopmentConfig")
-
->>>>>>> aac06878b7a1a810ea638f269f262f646de801d0
 from app import views
